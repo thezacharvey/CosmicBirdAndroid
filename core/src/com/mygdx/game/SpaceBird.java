925ad1c1fa8cd7b0a9356d065c9e9e4f5e12c1d2
zac <z_ig_ster777@hotmail.com>
2016-11-08 21:15:25 -0700

    Working Star randomizing, no overlapping star Y

--- core/src/com/mygdx/game/SpaceBird.java
+++ core/src/com/mygdx/game/SpaceBird.java
@@ -2,11 +2,13 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.mygdx.game.Sprites.Star;
 
 public class SpaceBird extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -14,29 +16,40 @@ public class SpaceBird extends ApplicationAdapter {
 	private Animation animation;
 	private Texture texture;
 	private OrthographicCamera camera;
+	private Star star;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-
 		texture = new Texture("bird.png");
 		animation = new Animation(new TextureRegion(texture),3,0.25f);  //passes texture , frame count, speed
 
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false,Gdx.graphics.getWidth()/20,Gdx.graphics.getHeight()/20);
+
+		star = new Star(camera);
+
+
 	}
 
 	@Override
 	public void render () {
 		animation.update(Gdx.graphics.getDeltaTime());
-
+		update(Gdx.graphics.getDeltaTime());
+		star.render();
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.setProjectionMatrix(camera.combined);
-		batch.draw(animation.getFrame(), 0, 0);
+		batch.draw(animation.getFrame(), 0, camera.viewportHeight/2 - animation.getFrame().getRegionHeight()/2);
+		batch.draw(star.getTextureRegion(), star.getX(), star.getY());
 		batch.end();
 
+
+	}
+
+	public void update(float dt)
+	{
 
 	}
 	
---
+++ core/src/com/mygdx/game/Sprites/Star.java
@@ -0,0 +1,104 @@
+package com.mygdx.game.Sprites;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Circle;
+import com.mygdx.game.Animation;
+import com.mygdx.game.SpaceBird;
+
+import java.util.Random;
+
+/**
+ * Created by z_ig_ on 11/8/2016.
+ */
+
+public class Star extends SpaceBird {
+
+    private Texture star;
+    private Animation animation;
+    static float starX;
+    private float cameraWidth;
+    private float cameraHeight;
+    private float velocity;
+    private float starWidth;
+    private float starHeight;
+    private int intHeight;
+    private Circle circle;
+    private float newStarY;
+    private Random random;
+   // private Sprite sprite;
+
+    public Star(OrthographicCamera camera) {
+
+        random =new Random();
+        intHeight = (int)camera.viewportHeight;
+        intHeight = random.nextInt(intHeight);
+
+        cameraWidth = camera.viewportWidth;
+        cameraHeight = camera.viewportHeight;
+
+
+        star = new Texture("star.png");
+        animation = new Animation(new TextureRegion(star), 2, 0.35f);
+        starHeight = animation.getFrame().getRegionHeight();
+        starWidth = animation.getFrame().getRegionWidth();
+        starX = cameraWidth + starWidth;
+        newStarY = starY(0);
+        velocity = cameraWidth / 2;
+        circle = new Circle();
+       // circle.set(starX,starY,starWidth/2);
+
+    }
+
+
+
+    @Override
+    public void render() {
+        update(Gdx.graphics.getDeltaTime());
+        animation.update(Gdx.graphics.getDeltaTime());
+    }
+
+    @Override
+    public void update(float dt) {
+        if (starX <= -starWidth)
+        {
+            newStarY = starY(newStarY);
+            starX = cameraWidth+animation.getFrame().getRegionWidth();
+        }
+        starX-=velocity*dt;
+    }
+
+    private int starY(float prev)
+    {
+        int prevY =(int) prev;
+        int starY  = random.nextInt((int)cameraHeight);
+        if (prevY==starY) {starY += starHeight;}
+        int camH = (int)cameraHeight;
+        int starH = (int)starHeight;
+        if (starY >= camH-starH) {
+            starY = camH - starH;
+            if (prevY==starY) starY-=starH;
+        }
+        if (starY <= starH) {
+            starY = starH;
+            if (prevY==starY) starY+=starH;
+        }
+
+        return starY;
+    }
+
+    @Override
+    public void dispose() {
+        star.dispose();
+    }
+
+    public TextureRegion getTextureRegion() {return animation.getFrame();}
+    public float getX(){return starX;}
+    public float getWidth(){return starWidth;}
+    public float getHeight(){return starHeight;}
+    public float getY(){return newStarY;}
+}
